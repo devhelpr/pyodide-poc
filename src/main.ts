97bd3f63c1c46be0c1eed0ce99d0a2e258bdc524
Maikel van de Lisdonk <maikel@devhelpr.com>
2024-07-19 17:04:32 +0200

    pyodide in worker

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,32 @@
 import "./style.css";
-import { loadPyodide } from "pyodide";
+//import { loadPyodide } from "pyodide";
+const workerFactory = (workerScript: URL, workerOptions: WorkerOptions) => () =>
+  new Worker(workerScript, workerOptions);
 
-async function runPytonCode() {
-  let pyodide = await loadPyodide({ indexURL: "pyodide" }); //indexURL: "pyodide",
-  await pyodide.loadPackage(["numpy", "scikit-learn"]);
-  return pyodide.runPythonAsync(`import numpy as np
-from sklearn.cluster import k_means
-X = np.array([[1, 2], [1, 4], [1, 0], [10, 2], [10, 4], [10, 0]])
-centroid, label, inertia = k_means(X, n_clusters=2, n_init="auto", random_state=0)
-centroid
-`);
-}
+const main = () => {
+  const factory = workerFactory(new URL("./worker.ts", import.meta.url), {
+    type: "module",
+  });
+  const worker = factory();
 
-runPytonCode().then((result) => {
-  console.log("result =", result.toString());
-});
+  worker.onmessage = (e) => console.log(e);
+  worker.onerror = (e) => console.error(e);
+  worker.postMessage("Hello world");
+};
+
+main();
+
+// async function runPytonCode() {
+//   let pyodide = await loadPyodide({ indexURL: "pyodide" }); //indexURL: "pyodide",
+//   await pyodide.loadPackage(["numpy", "scikit-learn"]);
+//   return pyodide.runPythonAsync(`import numpy as np
+// from sklearn.cluster import k_means
+// X = np.array([[1, 2], [1, 4], [1, 0], [10, 2], [10, 4], [10, 0]])
+// centroid, label, inertia = k_means(X, n_clusters=2, n_init="auto", random_state=0)
+// centroid
+// `);
+// }
+
+// runPytonCode().then((result) => {
+//   console.log("result =", result.toString());
+// });
---
+++ src/worker.ts
@@ -0,0 +1,19 @@
+import { loadPyodide } from "pyodide";
+self.onmessage = async (e: MessageEvent<any>) => {
+  console.log("Worker got message", e.data);
+  setTimeout(() => postMessage("Hello back"), 1000);
+  async function runPytonCode() {
+    let pyodide = await loadPyodide({ indexURL: "/pyodide" }); //indexURL: "pyodide",
+    await pyodide.loadPackage(["numpy", "scikit-learn"]);
+    return pyodide.runPythonAsync(`import numpy as np
+from sklearn.cluster import k_means
+X = np.array([[1, 2], [1, 4], [1, 0], [10, 2], [10, 4], [10, 0]])
+centroid, label, inertia = k_means(X, n_clusters=2, n_init="auto", random_state=0)
+centroid
+`);
+  }
+
+  runPytonCode().then((result) => {
+    console.log("result =", result.toString());
+  });
+};
