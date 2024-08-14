dfeaa2174a0d99c41a8129be9b45ad98be28b51a
Maikel van de Lisdonk <maikel@devhelpr.com>
2024-08-14 11:13:59 +0200

    integrated tailwind and split up initialising and starting the python code: python code needs to be started by pressing the start button.. it can also be restarted

--- src/App.tsx
+++ src/App.tsx
@@ -5,39 +5,59 @@ const workerFactory = (workerScript: URL, workerOptions: WorkerOptions) => () =>
 
 export const App = () => {
   const workerRef = useRef<Worker | undefined>(undefined);
-  const [result, setResult] = useState<string[]>([]);
+  const [result, setResult] = useState<string>("");
   const [loader, setLoader] = useState<string>("");
+  const [showButton, setShowButton] = useState<boolean>(false);
   useEffect(() => {
     setLoader("");
-    setResult([]);
+    setShowButton(false);
+    setResult("");
     const factory = workerFactory(new URL("./worker.ts", import.meta.url), {
       type: "module",
     });
     workerRef.current = factory();
     workerRef.current.onmessage = (e) => {
-      if (e.data.type && e.data.type === "result") {
-        let result: string[] = e.data.result;
-        let output = "";
-        result.forEach((result) => {
-          output += result + "\n";
-          console.log(result.toString());
-        });
-        setResult(result);
+      if (e.data.type && e.data.type === "initialised") {
+        setShowButton(true);
         setLoader("hidden");
+      } else if (e.data.type && e.data.type === "result") {
+        const outputResult: string[] = e.data.result;
+        let output = "";
+        outputResult.forEach((result) => {
+          output += result + "\n\r";
+        });
+        setResult(output);
+        setLoader("hidden");
+        setShowButton(true);
       } else {
         console.log(e);
       }
     };
     workerRef.current.onerror = (e) => console.error(e);
-    workerRef.current.postMessage("start");
+    workerRef.current.postMessage("init");
     return () => {
       workerRef.current?.terminate();
     };
   }, []);
+
+  const buttonClickHandler = () => {
+    setLoader("");
+    setResult("");
+    setShowButton(false);
+    workerRef.current?.postMessage("start");
+  };
   return (
     <>
       <div className={`loader ${loader}`}></div>
-      <div className="result">{result}</div>
+      <button
+        className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${
+          showButton ? "" : "hidden"
+        }`}
+        onClick={buttonClickHandler}
+      >
+        start
+      </button>
+      <div className="whitespace-pre-wrap font-mono">{result}</div>
     </>
   );
 };
--- src/worker.ts
+++ src/worker.ts
@@ -1,42 +1,61 @@
-import { loadPyodide } from "pyodide";
+import { loadPyodide, PyodideInterface } from "pyodide";
 import { demoCode } from "./demo-example";
 
 interface CustomWorker extends Worker {
   data: string;
   setResult: (result: string) => void;
+  pyodide: PyodideInterface | undefined;
 }
 declare var self: CustomWorker;
 
 self.onmessage = async (e: MessageEvent<any>) => {
   console.log("Worker got message", e.data);
-
-  const data = await fetch("/test_pred_BERT.csv").then((response) =>
-    response.text()
-  );
-  self.data = data;
   let resultList: string[] = [];
   self.setResult = (...result) => {
     resultList.push(result.join(" "));
   };
 
-  async function runPytonCode() {
-    let pyodide = await loadPyodide({ indexURL: "/pyodide" });
-    await pyodide.loadPackage(["micropip", "numpy", "pandas", "scikit-learn"]);
+  const data = await fetch("/test_pred_BERT.csv").then((response) =>
+    response.text()
+  );
+  self.data = data;
 
-    const micropip = pyodide.pyimport("micropip");
+  if (e.data === "init") {
+    await initPython();
+    postMessage({ type: "initialised" });
+  }
+  if (e.data === "start") {
+    await runPytonCode().then((_result) => {
+      console.timeEnd("pyodide-python");
+      postMessage({ type: "result", result: resultList });
+    });
+  }
+
+  async function initPython() {
+    self.pyodide = await loadPyodide({ indexURL: "/pyodide" });
+    await self.pyodide.loadPackage([
+      "micropip",
+      "numpy",
+      "pandas",
+      "scikit-learn",
+    ]);
+
+    const micropip = self.pyodide.pyimport("micropip");
     await micropip.install("unsupervised-bias-detection");
     await micropip.install("kmodes");
+    return true;
+  }
+
+  async function runPytonCode() {
+    if (!self.pyodide) {
+      throw new Error("Pyodide is not loaded");
+    }
 
     // use "python3 -m build" to create the wheel file from within the pip package root directory
     console.log("Start python code");
     console.time("pyodide-python");
 
-    return pyodide.runPythonAsync(demoCode);
+    return await self.pyodide.runPythonAsync(demoCode);
   }
-
-  runPytonCode().then((_result) => {
-    console.timeEnd("pyodide-python");
-    postMessage({ type: "result", result: resultList });
-  });
 };
 export {};
